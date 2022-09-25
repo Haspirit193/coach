fd68ce77185a5e622e823147cb95ab7c4ec1ecd3
Haspirit-lotus0 <hasutarou@gmail.com>
2022-09-25 11:34:30 +0900

    ファイル修正

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '64104fba3dee152b63cbec041a7ec7ca56de51b0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '64104fba3dee152b63cbec041a7ec7ca56de51b0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
